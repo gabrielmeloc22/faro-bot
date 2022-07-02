0c934a5a5a1ed6ce1b766d3d8b6d4b000fe75d8b
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-07-02 14:38:23 -0300

    refactor cantada command

--- src/commands/cantada.ts
+++ src/commands/cantada.ts
@@ -1,61 +1,38 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import {
-  CommandInteraction,
-  MessageActionRow,
-  MessageButton,
-  MessageEmbed,
-} from "discord.js";
+import { CommandInteraction } from "discord.js";
+import { buttons } from "./cantada/buttons";
+import { cantadaEmbed } from "./cantada/embed";
+
+const data = new SlashCommandBuilder()
+  .setName("cantada")
+  .setDescription("Uma escolha ousada")
+  .addUserOption((option) =>
+    option.setName("user").setDescription("O(a) pretendente").setRequired(true)
+  )
+  .addStringOption((option) =>
+    option.setName("cantada").setDescription("Sua cantada").setRequired(true)
+  );
+
+const execute = async (interaction: CommandInteraction) => {
+  const cantada = interaction.options.getString("cantada");
+  const { id: userId } = interaction.options.getUser("user") ?? {};
+
+  if (!cantada || !userId) return;
+
+  await interaction.reply({
+    content: "CAVALO",
+    ephemeral: true,
+  });
+
+  const message = await interaction.channel?.send({
+    embeds: [cantadaEmbed(cantada, userId)],
+    components: [buttons],
+  });
+
+  return message;
+};
 
 export default {
-  data: new SlashCommandBuilder()
-    .setName("cantada")
-    .setDescription("Uma escolha ousada")
-    .addUserOption((option) =>
-      option
-        .setName("user")
-        .setDescription("O(a) pretendente")
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option.setName("cantada").setDescription("Sua cantada").setRequired(true)
-    ),
-  async execute(interaction: CommandInteraction) {
-    const cantada = interaction.options.getString("cantada");
-    const userId = interaction.options.getUser("user")?.id;
-
-    await interaction.reply({
-      content: "CAVALO",
-      ephemeral: true,
-    });
-    if (!cantada || !userId) return;
-    const message = await interaction.channel?.send({
-      embeds: [cantadaEmbed(cantada, userId)],
-      components: [buttons],
-    });
-
-    return message;
-  },
+  data,
+  execute,
 };
-
-const cantadaEmbed = (cantada: string, userId: string) => {
-  return new MessageEmbed()
-    .setColor("DARK_VIVID_PINK")
-    .setTitle("Vai dar namoro?")
-    .setFields(
-      { name: "A cantada", value: cantada },
-      { name: "O(a) pretendente", value: `<@${userId}>` }
-    )
-    .setImage("https://c.tenor.com/gkak5SLi5mYAAAAM/torcendo-sorte.gif");
-};
-
-const buttons = new MessageActionRow().addComponents(
-  new MessageButton()
-    .setCustomId("accept")
-    .setLabel("Aceitar 💘")
-    .setStyle("SECONDARY"),
-
-  new MessageButton()
-    .setCustomId("decline")
-    .setLabel("Rejeitar 😭")
-    .setStyle("SECONDARY")
-);
