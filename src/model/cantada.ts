80d09d9cd70fc3050952ccdba443e039db4658a1
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-07-02 15:50:36 -0300

    database(cantada): add discordId field

--- src/model/cantada.ts
+++ src/model/cantada.ts
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const cantadaSchema = new mongoose.Schema(
   {
     body: String,
-    author: { type: "ObjectId", ref: "User" },
-    target: { type: "ObjectId", ref: "User" },
+    author: { type: "ObjectId", ref: "User", required: true },
+    target: { type: "ObjectId", ref: "User", required: true },
     status: { type: String, default: "PENDING" },
+    discordId: { type: String, unique: true, required: true },
   },
   { timestamps: true }
 );
--- src/services/user/getOrCreate.ts
+++ src/services/user/getOrCreate.ts
@@ -1,12 +1,12 @@
 import { User } from "../../model/user";
-import { CreateArgs } from "./create";
+import { create, CreateArgs } from "./create";
 
 export const getOrCreate = async (data: CreateArgs) => {
   try {
     const user = await User.findOne({ discordId: data.discordId });
 
     if (!user) {
-      const result = await User.create(data);
+      const result = await create(data);
       return result;
     }
 
